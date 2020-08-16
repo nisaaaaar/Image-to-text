4d9870e0e5fe2b61bcd42b1ab9b908619b365ce9
Nisar <nisara305@gmail.com>
2020-08-16 00:12:05 -0500

    Updates

---
+++ precache-manifest.b58f347580664ad39eae317c210a05e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cf7416388a2f15ada56566fd477e19",
+    "url": "/imageText/index.html"
+  },
+  {
+    "revision": "efd39d152b65962afdab",
+    "url": "/imageText/static/css/main.e29ed458.chunk.css"
+  },
+  {
+    "revision": "ae65ae6f2b4a55bf1486",
+    "url": "/imageText/static/js/2.b52002e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imageText/static/js/2.b52002e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd39d152b65962afdab",
+    "url": "/imageText/static/js/main.988a008f.chunk.js"
+  },
+  {
+    "revision": "c3f6c0f2daf6c4475650",
+    "url": "/imageText/static/js/runtime-main.80ca19c2.js"
+  }
+]);
